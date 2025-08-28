a680c8cef268539c08f46ec88ad2fbdacb9c4bdd
Maamb0ng <joshuamaambong398@gmail.com>
2025-08-28 11:43:19 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_8LN_h4N8brVtvoot5-4pU0n2BBxLFks",
+  authDomain: "pjm-goal-tracker.firebaseapp.com",
+  projectId: "pjm-goal-tracker",
+  storageBucket: "pjm-goal-tracker.firebasestorage.app",
+  messagingSenderId: "918212374551",
+  appId: "1:918212374551:web:e8924e154961a93587ac52"
+};
+
+
+const app = initializeApp(firebaseConfig);
